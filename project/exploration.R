9aae8dd6e2ba534cc89a3d54f2271b842687f1f7
Tobias Richter <tr4711@gmail.com>
2017-12-04 16:34:00 +0000

    Add some statistics and point out possible pitfalls

--- project/exploration.R
+++ project/exploration.R
@@ -11,24 +11,37 @@ plans <- plans[plans$BusinessYear == "2016", ]
 
 summary(rates)
 head(rates$PlanId)
+head(rates$IssuerId)
+paste("Benefits:", length(unique(rates$IssuerId)), "different issuers (ids)")
 paste("Rates:", length(unique(rates$PlanId)), "different plans (ids)")
+summary(rates$IndividualRate) # Strange value for some rates >9000 --> exclude from analysis
 
 
 summary(benefits)
 head(benefits$BenefitName)
 head(benefits$PlanId)
+head(benefits$IssuerId)
+paste("Benefits:", length(unique(benefits$IssuerId)), "different issuers (ids)")
 paste("Benefits:", length(unique(benefits$PlanId)), "different plans (ids)")
 
+networks <- networks[, c('NetworkId', 'StateCode', 'NetworkName', 'IssuerId')]
 summary(networks)
-head(networks$NetworkName, 20)
-head(networks$NetworkId, 50)
-
-# Networks == Plans?
+head(networks,20)
 paste("Networks:", length(unique(networks$NetworkId)), "different network (ids)")
+paste("Networks:", length(unique(networks$IssuerId)), "different issuers (ids)")
 paste("Networks:", length(networks$NetworkId), "number of networks in dataset (why doubles)")
+
+# Why double entries with same id and same name? ('Ameritas PPO Dental Network') --> IssuerId different
+# Why does IssuerId matters within NetWorks?
+aln001 <- networks[networks$NetworkId == 'ALN001',]
+head(aln001, 50)
 
 
 summary(plans)
 head(plans$PlanId, 50)
 head(plans$NetworkId, 50)
-
+head(plans$IssuerId, 50)
+head(plans$BenefitPackageId, 50)
+paste("Plans:", length(unique(plans$PlanId)), "different plans")
+paste("Plans:", length(unique(plans$NetworkId)), "number of networks associated")
+paste("Plans:", length(unique(plans$IssuerId)), "different issuers (ids)")
