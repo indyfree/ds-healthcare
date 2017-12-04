ce4a801071c39af9dc043ed9fe475e17874060a8
Tobias Richter <tr4711@gmail.com>
2017-12-04 13:16:24 +0000

    Add R script for dataset exploration

---
+++ project/exploration.R
@@ -0,0 +1,28 @@
+# Data exploration Scripts and first Visualisations
+
+# Set working directory to current file location (requires Rstudio as an IDE)
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+rates <- read.csv(file = file.path("data", "rates2016.csv"), header = T, stringsAsFactors = F)
+benefits <- read.csv(file = file.path("data", "benefits2016.csv"), header = T, stringsAsFactors = F)
+networks <- read.csv(file = file.path("data", "networks2016.csv"), header = T, stringsAsFactors = F)
+
+
+summary(rates)
+head(rates$PlanId)
+paste("Rates:", length(unique(rates$PlanId)), "different plans (ids)")
+
+
+summary(benefits)
+head(benefits$BenefitName)
+head(benefits$PlanId)
+paste("Benefits:", length(unique(benefits$PlanId)), "different plans (ids)")
+
+summary(networks)
+head(networks$NetworkName, 20)
+head(networks$NetworkId, 50)
+
+# Networks == Plans?
+paste("Networks:", length(unique(networks$NetworkId)), "different network (ids)")
+paste("Networks:", length(networks$NetworkId), "number of networks in dataset (why doubles)")
+
+
