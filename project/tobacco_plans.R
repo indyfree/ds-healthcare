3bc330b13c8cafc9673100afcc8dfc940cd53bc7
Tobias Richter <tr4711@gmail.com>
2017-12-30 10:15:15 +0000

    Clean up feature pipline

--- project/tobacco_plans.R
+++ project/tobacco_plans.R
@@ -6,35 +6,36 @@ rates <- read.csv(file = file.path("data", "rates2016.csv"), header = T, strings
 pol <- read.csv(file = file.path("data", "political_orientation.csv"), header = T, stringsAsFactors = F)
 
 # 1. Find Abortion Benefits
-print(length(unique(benefits$StateCode)))
 abortion_benefits <- subset(benefits, grepl("Abortion for Which Public", benefits$BenefitName)) 
-a <- aggregate(abortion_benefits$BenefitName, list(abortion_benefits$StateCode), length)
 
 # 2. Find plans that cover these benefits
 abortion_plans <- subset(plans, plans$PlanId %in% abortion_benefits$PlanId)
-b <- aggregate(abortion_plans$PlanId, list(abortion_plans$StateCode), length)
 
-# 3. Find Ration of Plans that cover Abortion
-c <- aggregate(plans$PlanId, list(plans$StateCode), length)
-d <- merge(b,c,by="Group.1")
-colnames(d) <- c('State', 'AbortionPlans', 'TotalPlans')
-d$Ratio <- d$AbortionPlans/d$TotalPlans
+# 3. Count Abortion Plans per State
+states <- aggregate(abortion_plans$PlanId, list(abortion_plans$StateCode), length)
+colnames(states) <- c('State', 'NumAbortionPlans')
 
-# 4. Merge political orientation
-d <- merge(d, pol, by = 'State')
-ggplot(data=d, aes(x=State, y=Ratio, fill=factor(Color))) +
-  geom_bar(stat="identity", position=position_dodge())
+# 4. Find Ratio of Plans that cover Abortion
+num_total_plans <- aggregate(plans$PlanId, list(plans$StateCode), length)
+colnames(num_total_plans) <- c('State', 'NumPlans')
+num_total_plans <- subset(num_total_plans,num_total_plans$State %in% states$State)
+states$RelAbortionPlans <- states$NumAbortionPlans/num_total_plans$NumPlans
 
+# 4. Merge political orientation (Label)
+states <- merge(states, pol, by = 'State')
+# ggplot(data=d, aes(x=State, y=Ratio, fill=factor(Color))) +
+#   geom_bar(stat="identity", position=position_dodge())
 
-# 5. Compare plans payments within the states, mean vs. abortion
+# 5. Compute Mean Rates for Abortion Plans
 abortion_rates <- subset(rates, rates$PlanId %in% abortion_plans$StandardComponentId)
-
 abortion_rates <- abortion_rates[,c('StateCode', 'IndividualRate')]
 mean_abortion_rates <- aggregate(abortion_rates, list(abortion_rates$StateCode), mean)
+states$MeanAbortionRates <- mean_abortion_rates$IndividualRate
 
+# 6. ExtraPay = Difference between mean rate of all planes and the ones with abortion benefits
 mean_rates <- rates[,c('StateCode', 'IndividualRate')]
 mean_rates <- aggregate(mean_rates, list(mean_rates$StateCode), mean)
-d$ExtraPay <- (mean_abortion_rates$IndividualRate - mean_rates$IndividualRate)
+states$ExtraPay <- (mean_abortion_rates$IndividualRate - mean_rates$IndividualRate)
 
 ggplot(data=d, aes(x=State, y=ExtraPay, fill=factor(Color))) +
   geom_bar(stat="identity", position=position_dodge())
